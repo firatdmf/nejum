ac0b3becaf16cc2599fe513aa122883572466208
Muhammed Firat Ozturk <firatdmf@gmail.com>
2024-10-09 14:06:13 +0300

    created the app

---
+++ nejum/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="HomePage">
+      <div className="header bg-[#f1f1f1] h-8 shadow-xl"></div>
+    </div>
+  );
+}
