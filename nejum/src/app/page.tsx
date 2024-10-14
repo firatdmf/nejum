37dad620b564476b7359627db3d7b1a0b2331a33
Muhammed Firat Ozturk <firatdmf@gmail.com>
2024-10-14 08:49:41 +0300

    committing last changes

--- nejum/src/app/page.tsx
+++ nejum/src/app/page.tsx
@@ -1,9 +1,61 @@
 import Image from "next/image";
-
+import classes from "./page.module.css";
+import Link from "next/link";
 export default function Home() {
+  let article: String = "Selecting the right product metrics (KPIs)";
   return (
     <div className="HomePage">
-      <div className="header bg-[#f1f1f1] h-8 shadow-xl"></div>
+      <div className={`${classes.header} bg-[#f1f1f1] shadow-xl `}>
+        <div className={classes.left}>
+          <div className={classes.logoText}>Nejum</div>
+          <p>
+            Nejum »{" "}
+            <Link rel="stylesheet" href="">
+              {article}
+            </Link>
+          </p>
+        </div>
+        <div className={classes.right}>
+          <Link rel="stylesheet" href="">
+            All Articles
+          </Link>
+          <Link rel="stylesheet" href="">
+            Author
+          </Link>
+        </div>
+      </div>
+      <div className={classes.body}>
+        <div className={classes.blog}>
+          <div className={classes.readTime}>
+            July 16, 2023 <br />
+            Reading time: 12 min <br />
+            PDF Logo Printable PDF <br />
+          </div>
+          <article>
+            <h1>Selecting the right product metrics (KPIs)</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ex
+              sunt! Eos nihil aspernatur, numquam ullam ipsam accusamus deleniti
+              incidunt minima alias in sequi earum dolorem aliquam consequatur
+              modi libero?
+            </p>
+            <h2>title 2</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+              quam mollitia assumenda. Iure commodi hic quis. Debitis beatae
+              voluptates quo dolores corporis aspernatur maxime iusto reiciendis
+              eos, minima, nam recusandae!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+              quam mollitia assumenda. Iure commodi hic quis. Debitis beatae
+              voluptates quo dolores corporis aspernatur maxime iusto reiciendis
+              eos, minima, nam recusandae!
+            </p>
+          </article>
+        </div>
+        <div className={classes.share}>Hello</div>
+      </div>
     </div>
   );
 }
