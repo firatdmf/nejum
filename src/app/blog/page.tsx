c1496182a79d5c5532666f026d3c514ffd131566
Muhammed Firat Ozturk <firatdmf@gmail.com>
2024-11-14 16:10:07 +0300

    latest changes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import { IoSearchSharp } from "react-icons/io5";
 export default function Blog() {
   return (
     <>
@@ -8,9 +9,28 @@ export default function Blog() {
           <h1 className={`${classes.headline} text-3xl font-bold"`}>
             Welcome to Nejum
           </h1>
-          <p className="italic">
+          <p className={`${classes.littleTitle} "italic"`}>
             Articles on Leveraging Technology for Smarter Business Operations
           </p>
+          <div className={classes.sort_and_search_bar}>
+            <div className={classes.sort}>
+              <p>Sort By:</p>
+              <button>Popularity</button>
+              <button>Newest</button>
+            </div>
+            <div className={classes.search}>
+              <div className={classes.search_icon}>
+              <IoSearchSharp />
+              </div>
+              <input
+                type="text"
+                maxLength={64}
+                aria-label="Search"
+                placeholder="Search"
+                id="search_input"
+              />
+            </div>
+          </div>
           <div className={classes.blog_grid_container}>
             <div className={classes.blog_grid_item}>
               <Link href="/blogs/article_title">
@@ -38,7 +58,7 @@ export default function Blog() {
               <Link href="/blogs/article_title">
                 <div className={classes.blog_grid_item_image}>
                   <img
-                    src="https://longform.asmartbear.com/product-market-fit-formula/thumbnail-1200w_huc583e90880d0798f52d60c98903e3979_137961_360x0_resize_q75_h3_mitchellnetravali_3.webp"
+                    src="https://longform.asmartbear.com/problem/thumbnail-1200w_hu959ef7dc51c83d24d3771e11fce8f8bc_412484_360x0_resize_q75_h3_mitchellnetravali_3.webp"
                     alt=""
                   />
                 </div>
